d4844d8f23099b26624a0fee339d0ea6814d156f
lmeller-git <louis.meller@icloud.com>
2024-12-10 13:41:17 +0100

    day10 part 2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -4,13 +4,13 @@ use std::{collections::HashSet, path::PathBuf};
 type HeightMap = Vec<Vec<u8>>;
 type Position = (usize, usize);
 
-pub fn _main(data: PathBuf, out: PathBuf, verbosity: u8) -> Result<()> {
+pub fn _main(data: PathBuf, _out: PathBuf, verbosity: u8) -> Result<()> {
     let height_map = parse(data)?;
     if verbosity > 2 {
         print_heightmap(&height_map);
     }
-    let res1 = solve(&height_map);
-    println!("res1: {res1}");
+    let res = solve(&height_map);
+    println!("res1: {}, res2: {}", res.0, res.1);
     Ok(())
 }
 
@@ -51,27 +51,40 @@ fn next_path(heights: &HeightMap, start: Position) -> Vec<Position> {
     next_fields
 }
 
-fn get_paths(heights: &HeightMap, start: Position) -> Vec<Position> {
+fn get_paths_unique(heights: &HeightMap, start: Position) -> Vec<Position> {
     if heights[start.0][start.1] == 9 {
         return vec![start];
     }
     let mut tot = HashSet::new();
     for next_start in next_path(heights, start) {
-        for p in get_paths(heights, next_start) {
+        for p in get_paths_unique(heights, next_start) {
             tot.insert(p);
         }
     }
     tot.into_iter().collect::<Vec<Position>>()
 }
 
-fn solve(heights: &HeightMap) -> usize {
+fn get_paths(heights: &HeightMap, start: Position) -> usize {
+    if heights[start.0][start.1] == 9 {
+        return 1;
+    }
     let mut tot = 0;
+    for next_start in next_path(heights, start) {
+        tot += get_paths(heights, next_start);
+    }
+    tot
+}
+
+fn solve(heights: &HeightMap) -> (usize, usize) {
+    let mut tot = 0;
+    let mut tot2 = 0;
     for (i, row) in heights.iter().enumerate() {
         for (j, height) in row.iter().enumerate() {
             if *height == 0 {
-                tot += get_paths(heights, (i, j)).len();
+                tot += get_paths_unique(heights, (i, j)).len();
+                tot2 += get_paths(heights, (i, j));
             }
         }
     }
-    tot
+    (tot, tot2)
 }
