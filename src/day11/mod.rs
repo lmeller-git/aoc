16eb408e1bffe8f81787c70563f89c4e2561b0b0
lmeller-git <louis.meller@icloud.com>
2024-12-11 15:36:06 +0100

    day 11 part 2

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,56 +1,66 @@
 use super::{AOCError, Result};
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 pub fn _main(data: PathBuf, _out: PathBuf, _verbosity: u8) -> Result<()> {
-    let stones = parse(data)?;
-    let res = solve(stones);
-    println!("res: {}", res);
+    let stones2 = parse_hash(&data)?;
+    let res2 = solve_efficient(stones2, 75);
+    let stones = parse_hash(&data)?;
+    let res = solve_efficient(stones, 25);
+    println!("res: {}, res2: {}", res, res2);
     Ok(())
 }
 
-type Stones = Vec<u64>;
+type HashStones = HashMap<u64, usize>;
 
-fn parse(data: PathBuf) -> Result<Stones> {
+fn parse_hash(data: &PathBuf) -> Result<HashStones> {
     let f = std::fs::read_to_string(data)?;
-    f.split_whitespace()
-        .map(|num| {
-            num.parse::<u64>()
-                .map_err(|_e| AOCError::ParseError("could not parse num".into()))
-        })
-        .collect::<Result<Stones>>()
-}
-
-fn solve(mut stones: Stones) -> usize {
-    for _ in 0..25 {
-        tick(&mut stones);
+    let mut stones = HashStones::new();
+    for stone in f.split_whitespace().map(|num| {
+        num.parse::<u64>()
+            .map_err(|_e| AOCError::ParseError("could not parse num".into()))
+    }) {
+        stones.entry(stone?).and_modify(|e| *e += 1).or_insert(1);
     }
-    stones.len()
+    Ok(stones)
 }
 
-fn tick(stones: &mut Stones) {
-    let mut i = 0;
-    while i < stones.len() {
-        if let Some(new_stone) = update_stone(stones.get_mut(i)) {
-            i += 1;
-            stones.insert(i, new_stone);
-        }
-        i += 1;
+fn solve_efficient(mut stones: HashStones, blinks: usize) -> usize {
+    for _ in 0..blinks {
+        tick_efficient(&mut stones);
     }
+    stones.values().sum()
 }
 
-fn update_stone(stone: Option<&mut u64>) -> Option<u64> {
-    if let Some(stone) = stone {
-        if *stone == 0 {
-            *stone = 1;
-        } else if (stone.ilog10() + 1) % 2 == 0 {
-            let div = 10_u64.pow((stone.ilog10() + 1) / 2);
-            let first_half = *stone / div;
-            let second_half = *stone % div;
-            *stone = first_half;
-            return Some(second_half);
-        } else {
-            *stone *= 2024;
+fn tick_efficient(stones: &mut HashStones) {
+    let mut new_entries = HashStones::with_capacity(stones.len());
+    for (value, num) in stones.iter() {
+        let mut current_value = *value;
+        if let Some(new_value) = update_stone(&mut current_value) {
+            new_entries
+                .entry(new_value)
+                .and_modify(|e| *e += *num)
+                .or_insert(*num);
         }
+        new_entries
+            .entry(current_value)
+            .and_modify(|e| *e += *num)
+            .or_insert(*num);
+    }
+    *stones = new_entries;
+}
+
+fn update_stone(stone: &mut u64) -> Option<u64> {
+    if *stone == 0 {
+        *stone = 1;
+    } else if (stone.ilog10() + 1) % 2 == 0 {
+        let div = 10_u64.pow((stone.ilog10() + 1) / 2);
+        let first_half = *stone / div;
+        let second_half = *stone % div;
+        *stone = first_half;
+        return Some(second_half);
+    } else {
+        *stone *= 2024;
     }
     None
 }
@@ -62,7 +72,7 @@ mod tests {
     #[test]
     fn split_stone() {
         let mut stone = 2010;
-        let stone2 = update_stone(Some(&mut stone));
+        let stone2 = update_stone(&mut stone);
         assert_eq!(stone2, Some(10));
         assert_eq!(stone, 20);
     }
